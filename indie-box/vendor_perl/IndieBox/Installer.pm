df9808a38fd2c041e913ab6e914e557c3eb66d79
Johannes Ernst <jernst@cldstr.com>
2013-09-12 12:21:29 -0700

    Adding a package installer

---
+++ indie-box/vendor_perl/IndieBox/Installer.pm
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+#
+# Represents a Host for Indie Box Project
+#
+# Copyright (C) 2013 Johannes Ernst
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+use strict;
+use warnings;
+
+package IndieBox::Installer;
+
+##
+# Invoked by pacman after this package has been installed for the first time
+# $newVersion: identifier of the installed package version
+sub post_install {
+    my $version = shift;
+
+}
+
+##
+# Invoked by pacman after this package has been upgraded
+# $newVersion: identifier of the new package version
+# $oldVersion: identifier of the old package version
+sub post_upgrade {
+    my $newVersion = shift;
+    my $oldVersion = shift;
+
+    # Nothing for now
+}
+
+##
+# Invoked by pacman before this package is being removed
+# $newVersion: identifier of the new package version
+# $oldVersion: identifier of the old package version
+sub pre_remove {
+    my $version = shift;
+
+    print <<MSG;
+
+Hey hacker,
+
+Sorry to see you go.
+Hope to see you again in the future.
+
+Regards,
+    Indie Box Project
+    http://indieboxproject.org/
+
+MSG
+}
+1;
